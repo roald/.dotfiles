24ee18a47d0214852ca44da612c1c9da38ba9fb1
Roald Dijkstra <roald.dijkstra@babbletics.nl>
2012-01-26 00:57:59 +0100

    status fix

--- bash/branch.sh
+++ bash/branch.sh
@@ -1,4 +1,7 @@
 # Default Git enabled prompt
-export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWDIRTYSTATE=true # '*' for unstaged changes, '+' for staged
+export GIT_PS1_SHOWSTASHSTATE=true # '$' if smth is stashed
+export GIT_PS1_SHOWUNTRACKEDFILES=true # '%' if un-tracked files
+
 export PS1="\[$txtgrn\]\u\[$txtrst\]: \w\[$txtred\]$(__git_ps1)\[$txtrst\] \$ "
 
