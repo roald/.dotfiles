e8b5b3ed41604c5f57b382d377fdc99ec705c1c9
Roald Dijkstra <roald.dijkstra@babbletics.nl>
2012-01-26 11:58:22 +0100

    git completion to branch file

--- main.sh
+++ main.sh
@@ -6,6 +6,5 @@ source ~/.dotfiles/bash/aliases.sh
 bind "set completion-ignore-case on"
 
 # branch information
-source ~/.dotfiles/bash/git-completion.sh
-# export PS1="\[\033[0;33m\]\u\[\033[00m\]:\[\033[00m\]\w\[\033[0;31m\] \`ruby -e \"print (%x{git branch 2> /dev/null}.grep(/^\*/).first || '').gsub(/^\* (.+)$/, '(\1) ')\"\`\[\033[00m\]$\[\033[00m\] "
 source ~/.dotfiles/bash/branch.sh
+
