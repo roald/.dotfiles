459e8c8b34dd67ce0f47b171deabed8c3cc5dac6
Roald Dijkstra <roald.dijkstra@babbletics.nl>
2012-10-05 11:56:53 +0200

    new folder aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,12 +1,29 @@
 # Using just the dots for moving up
 alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
 alias cd..="cd .."
 
 
+# Directory shortcuts
+alias ddrop="cd ~/Documents/Dropbox/Dropbox"
+alias ddesk="cd ~/Desktop"
+alias ddown="cd ~/Downloads"
+alias dweb="cd ~/Documents/webroot"
+
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+	colorflag="--color"
+else # OS X `ls`
+	colorflag="-G"
+fi
+
 # Colorful LS commands with some other presets
-alias ls="ls -FG"
-alias ll="ls -l"
-alias la="ls -AlG"
+alias ls="ls -F ${colorflag}"
+alias ll="ls -l ${colorflag}"
+alias la="ls -Al ${colorflag}"
+alias lsd='ls -l ${colorflag} | grep "^d"'
 
 
 # Previewing files
